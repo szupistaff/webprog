55ac7f46af471a384fdbeb9183bac944e1545782
szupistaff <bagolyvaok@gmail.com>
2025-09-29 09:50:51 +0200

    2

--- main.js
+++ main.js
@@ -8,11 +8,18 @@ window.addEventListener('load', (e) => {
 
     for (let ci=0; ci<size*size; ci++) {
         let cell = document.createElement('div');
-        cell.className = 'cell';
+        cell.classList.add('cell');
+        let sor = ci / size;
+        let oszlop = ci % 2;
+        if (ci % 2 == 0) {
+            cell.classList.add('black');
+        }
         cell.style.width = cellSize + 'px';
         cell.style.height = cellSize + 'px';
         grid.appendChild(cell);
+
     }
+
 
     document.body.appendChild(grid);
 });
